c4c2a385c6484b476be406890542d0ea20a6db94
Vlad <6847864@gmail.com>
2023-01-26 21:06:14 +0200

    added Search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '4f03edbfffmsh7a6729e58896fe1p1c6f3fjsn31c9794bdc89',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
---
+++ src/components/Current-weather/Current-weather.js
@@ -0,0 +1,7 @@
+import './current-weather.scss';
+
+const CurrentWeather = () => {
+  return <h1>'hello'</h1>;
+};
+
+export default CurrentWeather;
