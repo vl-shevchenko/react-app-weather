9df2992c374bcc0474d0b973800b0df7e2e44d80
Vlad <6847864@gmail.com>
2023-02-07 14:46:51 +0200

    changed the http protocol for REVERSE_API_URL

--- src/api.js
+++ src/api.js
@@ -16,6 +16,6 @@ export const TodayHourlyWeatherOptions = {
 
 export const TW_API_URL = 'https://weatherapi-com.p.rapidapi.com/forecast.json';
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
-export const REVERSE_API_URL = 'http://api.openweathermap.org/geo/1.0';
+export const REVERSE_API_URL = 'https://api.openweathermap.org/geo/1.0';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 export const WEATHER_API_KEY = '4dfaed9868ac707d3b6d0bf95b10aea1';
